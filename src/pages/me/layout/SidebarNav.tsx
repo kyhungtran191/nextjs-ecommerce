40d28736dd1fd7c5a64232496250b9c0731f1eb6
justinJSwebdev <trankyhung225@gmail.com>
2024-07-02 14:41:27 +0700

    update export deafult deploy error

--- src/pages/me/layout/SidebarNav.tsx
+++ src/pages/me/layout/SidebarNav.tsx
@@ -11,7 +11,7 @@ interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
   }[];
 }
 
-export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
+export default function SidebarNav({ className, items, ...props }: SidebarNavProps) {
   const pathname = usePathname();
 
   return (
